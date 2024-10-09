7ae5ad74a621c3be526c421f100e076e6ffac406
Marcorr03 <marcoanrr0314@gmail.com>
2024-10-08 22:06:19 -0600

    V1 Proyecto

---
+++ Proyecto_1_Locales/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Header,Registro,Login} from './Cliente.jsx'
+
+export function App() {
+
+  return (
+      <>
+        <Header/>
+        <Registro/>
+        <Login/>
+      </>
+    )
+  }
---
+++ Proyecto_1_Locales/src/funciones.jsx
@@ -0,0 +1,9 @@
+import {useState} from 'react';
+
+export function Agregar(No,Ap,Id){
+return(
+    <>
+    
+    </>
+)
+}
